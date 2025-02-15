1df6ea7e6de03c2aa2dcab5161ed79d87ee9f15f
Brian-Pob <brianpoblete77777@gmail.com>
2025-02-15 04:01:25 -0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import deno from "@deno/vite-plugin";
 
+const SERVER_URL = Deno.env.get("URL") ?? "http://localhost:8080";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [deno()],
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:8080",
+				target: SERVER_URL,
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ""),
 			},
